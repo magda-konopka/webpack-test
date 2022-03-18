6d3031c67f132113f0326ae93e5042fc9809f62d
Magda Konopka <madzia199652@interia.pl>
2022-03-18 14:28:46 +0100

    Change index.html and add styleMK.scss

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,26 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+const firstName = "Magda";
+const age = "25";
+
+console.log(firstName);
+console.log(age);
+console.log(`Cześć!Nazywam się ${firstName} i mam ${age}lat.`);
+const heading = document.querySelector('.hotpink-header--js');
+heading.innerHTML =`Cześć!Nazywam się ${firstName} i mam ${age}lat.` 
+
+greet(age, firstName);
+
+function greet(age,firstName) {
+    console.log(
+        `Cześć!Nazywam się ${firstName} i mam ${age}lat.`
+
+    );
+}
+
+const hamburger = document.querySelector('.hamburger--js');
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--open');
+})
